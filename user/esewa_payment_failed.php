f7788e34c80cebe29e12bd9b1397ae90138c69f1
Atul <47029796+atulpokharel-gp@users.noreply.github.com>
2021-07-24 13:41:08 +0545

    Add files via upload

---
+++ user/esewa_payment_failed.php
@@ -0,0 +1,14 @@
+<?php 
+    require_once('../Includes/config.php'); 
+    require_once('../Includes/session.php');
+    require_once('../Includes/user.php');
+
+    $uid = $_SESSION['uid'];
+
+//UPDATE BILL
+//UPDATE TRANSACTION
+
+    mysqli_query($con,"UPDATE `bill` SET `status` = 'FAILED' WHERE `bill`.`uid` = '$uid' and `bill`.`status`='PENDING';");
+       
+    header("Location:bill.php");
+?>
